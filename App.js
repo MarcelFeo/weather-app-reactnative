245d619fb72ada71c9ecadb729b176a463effdd9
MarcelFeo <marcelfeo29@gmail.com>
2022-02-02 15:01:59 -0300

    adicionando o expo-location

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    loading()
+  }, []);
+
+  // função de loading
+  async function loading() {
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+
+      if(status != 'granted') {
+        setErrorMessage('Acesso a lkocalização necessário para a execução do app');
+        return
+      }
+
+      // localização atual do usuário
+      const location = await Location.getCurrentPositionAsync();
+      const {latitude, longitude} = location.coords;
+
+      alert(`latitude: ${latitude} e longitude: ${longitude}`)
+
+    } catch (error) {}
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>weather app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#303C42',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  txt: {
+    color: '#fff',
   },
 });
