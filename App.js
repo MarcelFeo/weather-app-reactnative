c3ab09cde3ffc642e6e41677662d5bb744842ff9
MarcelFeo <marcelfeo29@gmail.com>
2022-02-07 15:36:10 -0300

    consumindo a api

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
+const WEATHER_API_KEY = 'ef4d6befb805f4ec72f0fa77a58f5bda';
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState('metric');
 
   useEffect(() => {
     loading()
@@ -13,10 +18,10 @@ export default function App() {
   // função de loading
   async function loading() {
     try {
-      let { status } = await Location.requestPermissionsAsync();
+      let { status } = await Location.requestForegroundPermissionsAsync();
 
       if(status != 'granted') {
-        setErrorMessage('Acesso a lkocalização necessário para a execução do app');
+        setErrorMessage('Acesso a localização necessário para a execução do app');
         return
       }
 
@@ -24,18 +29,46 @@ export default function App() {
       const location = await Location.getCurrentPositionAsync();
       const {latitude, longitude} = location.coords;
 
-      alert(`latitude: ${latitude} e longitude: ${longitude}`)
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`;
 
-    } catch (error) {}
+      // API Request
+      const response = await fetch(weatherUrl);
+      const result = await response.json();
+
+      if(response.ok) {
+        setCurrentWeather(result);
+      } else {
+        setErrorMessage(result.message);
+      }
+
+      // alert(`latitude: ${latitude} e longitude: ${longitude}`)
+
+    } catch (error) {
+      setErrorMessage(error);
+    }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.txt}>weather app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  if(currentWeather) {
+    // temperatura atual
+    const { main : {temp} } = currentWeather;
+    return (
+      <View style={styles.container}>
+        <View styles={style.}>
+
+        </View>
+        <Text style={styles.text}>Temperatura = {temp}°C</Text>
+        <StatusBar style="auto" />
+      </View>
+    )} else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.txt}>{errorMessage}</Text>
+          <StatusBar style="auto" />
+        </View>
+      )
+    }
+  }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -44,7 +77,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  txt: {
+  text: {
     color: '#fff',
+  },
+  main: {
+
   },
 });
